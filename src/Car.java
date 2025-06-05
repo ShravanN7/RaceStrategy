7a37ded56f771bb09ca022529706612b46f12177
Shravan <nall0020@flinders.edu.au>
2025-06-05 17:45:47 +0930

    two numbers of sum and checking

--- src/Car.java
+++ src/Car.java
@@ -5,10 +5,10 @@ public class Car {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter first inte  ddddger: ");
+        System.out.print("Enter first inte  ddaaaaddger: ");
         int a = scanner.nextInt();
 
-        System.out.print("Enter second intesssccsssger: ");
+        System.out.print("Enter second intesssssger: ");
         int b = scanner.nextInt();
 
         int sum = a + b;
